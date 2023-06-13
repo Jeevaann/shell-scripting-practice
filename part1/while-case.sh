bb962cda5c8b3ac75fed66991942266dbe08e6b7
Jeevan-Darapu <Jeevan-Darapu>
2023-06-13 12:05:04 +0000

    modified while-case.sh

--- part1/while-case.sh
+++ part1/while-case.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 while true
 do
-	read -p "select the 1,2,3 or q to quit: " choice
+	read -p "1. Show disk usage
+2. Show uptime
+3. Show users
+What would you like to do? (Enter q to quit): " choice
 	case $choice in 
 		1)
 			echo $(df -h)
+			echo
 			;;
 		2)
 			echo $(uptime)
+			echo
 			;;
 		3)
 			echo $(who)
+			echo
 			;;
 		q)
 			echo "Goodbye!"
+			echo
 			exit 1
 			;;
 		*)
 			echo "invalid choice"
+			echo
 			;;
 	esac
 done
